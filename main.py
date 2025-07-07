29f0d24710959eaf9722ce5361b2fc016ca37429
Allen <allensavage1222@gmail.com>
2025-07-08 01:59:37 +0800

    change output to the same dir with a postfix _output

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append("/home/userroot/GitClone/python-ffmpeg")
-sys.path.append("/home/userroot/OneDrive/Foam/MyThoughts/PC_Tech/python/own_function")
-from own_function import get_target_file_path
+sys.path.append("/home/userroot/GitClone/own_function")
+from own_function import target_file_path, target_file_path_with_level
 
 from downsampling import downsample_video
 from pathlib import Path
@@ -10,11 +10,13 @@ if __name__ == "__main__":
     print("start to convert...")
     target_ext = ".mp4"
     source_ext = ".mp4"
+    level = 1
 
     input_directory = "/home/userroot/shizheng/photo"
-    sub_dir = "output"
+    # sub_dir = "output"
+    name_postfix = "_output"
 
-    target_file_paths = get_target_file_path(input_directory, source_ext)
+    target_file_paths = target_file_path_with_level(input_directory, source_ext, level)
 
     count = 0
 
@@ -23,11 +25,11 @@ if __name__ == "__main__":
         count += 1
         print(count)
         file_path = str(file.resolve())
-        target_file = file.parent / sub_dir / file.name
+        target_file = file.parent / (file.stem + name_postfix + target_ext)
 
-        # create output folder if it does not existed
-        target_file.parent.mkdir(mode=755, parents=True, exist_ok=True)
+        # # create output folder if it does not existed
+        # target_file.parent.mkdir(mode=755, parents=True, exist_ok=True)
         
-        target_file_path = str(target_file.with_suffix(target_ext).resolve())
+        target_file_path = str(target_file.resolve())
 
         downsample_video(file_path, target_file_path)
